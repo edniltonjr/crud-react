b0f250b4842f4b54e66afe57414851a726e81669
Ednilton Junior <jrjunior_cg@live.com>
2020-03-10 14:36:50 -0300

    Finalizando estilização Main

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => (
+
+  <Main icon="home" title="Inicio"
+  subtitle="Projeto Crud!">
+
+    <div className="display-4">Bem vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema para exemplificar CRUD!</p>
+
+  </Main>
+
+)
